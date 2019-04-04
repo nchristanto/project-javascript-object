36045aea7cb67341742ada1c37bfcc088092437d
Chris <nandachristanto@ubuntu.ubuntu-domain>
2019-04-04 22:14:27 +0700

    Initialized index.css, index.html, index.js, README.md, and .gitignore

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log({ name: 'Alpha', age: 100, superPower: true })
+console.log({
+  name: 'Alpha',
+  age: 100,
+  superPower: true
+})
+
+const hero = { name: 'Alpha', age: 100, superPower: true }
+
+console.log(hero)
+console.log(hero['name'])
+console.log(hero.age)
+console.log(hero.superPower)
+
+alert(hero)
+alert(hero['name'])
+alert(hero.age)
+alert(hero.superPower)
